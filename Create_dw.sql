f4776b10d41cfee0747ea01114e4cc74a0192f68
BenjaminRousseau37416 <etu37416@henallux.be>
2023-01-07 15:55:43 +0100

    ETL Complet mais à Debug

--- Create_dw.sql
+++ Create_dw.sql
@@ -1,43 +1,58 @@
-CREATE DATABASE Foodies977Dw;
+USE Foodies977Dw;
 
+DROP TABLE factSales;
+DROP TABLE dimDate;
+DROP TABLE dimProduct;
+DROP TABLE dimCustomer;
+DROP TABLE dimEmployee;
+DROP TABLE dimCategory;
+DROP TABLE dimLocation;
+
+DROP TABLE IF EXISTS dimDate
 CREATE TABLE dimDate
 (
-    DateKey int IDENTITY(1,1) PRIMARY KEY,
-    OriginalId int NOT NULL,
-    Year int NOT NULL,
-    MonthEnglishName varchar(10) NOT NULL,
-    MonthFrenchName varchar(10) NOT NULL,
-    DayEnglishName varchar(10) NOT NULL,
-    DayFrenchName varchar(10) NOT NULL,
+    DateKey int IDENTITY(1,1) PRIMARY Key,
+	DayFrenchName nvarchar(100) NOT NULL,
+	DayEnglishName nvarchar(100) NOT NULL,
+	MonthFrenchName nvarchar(100) NOT NULL,
+	MonthEnglishName nvarchar(100) NOT NULL,
+	WeekNumber int NOT NULL,
+	DayOfYearNumber int NOT NULL,
+	DayOfWeekNumber int NOT NULL,
 );
 
+
+DROP TABLE IF EXISTS dimProduct
 CREATE TABLE dimProduct
 (
     ProductKey int IDENTITY(1,1) PRIMARY KEY,
     OriginalId int NOT NULL,
-    NameEnglish varchar(100) NOT NULL,
-    NameFrench varchar(100) NOT NULL,
-    QuantityPerUnit int NOT NULL,
+    productName nvarchar(40) NOT NULL,
+    QuantityPerUnit varchar(100) NOT NULL,
     Discontinued bit NOT NULL,
 );
 
+
+DROP TABLE IF EXISTS dimCustomer
 CREATE TABLE dimCustomer
 (
-    CustomerKey int IDENTITY(1,1) PRIMARY KEY,
-    OriginalId int NOT NULL,
-    CompanyName varchar(100) NOT NULL,
-    ContactName varchar(100) NOT NULL,
+    CustomerKey varchar(50) PRIMARY KEY,
+    [CompanyName] varchar(50),
+    [OriginalId] varchar(50),
+    [ContactName] varchar(50)
+
 );
 
+DROP TABLE IF EXISTS dimEmployee
 CREATE TABLE dimEmployee
 (
     EmployeeKey int IDENTITY(1,1) PRIMARY KEY,
     OriginalId int NOT NULL,
     LastName varchar(100) NOT NULL,
     FirstName varchar(100) NOT NULL,
-    Title varchar(100) NOT NULL,
 );
 
+DROP TABLE IF EXISTS dimCategory
 CREATE TABLE dimCategory
 (
     CategoryKey int IDENTITY(1,1) PRIMARY KEY,
@@ -46,26 +61,24 @@ CREATE TABLE dimCategory
     NameFrench varchar(100) NOT NULL,
 );
 
+DROP TABLE IF EXISTS dimLocation
 CREATE TABLE dimLocation
 (
-    LocationKey int IDENTITY(1,1) PRIMARY KEY,
-    OriginalId int NOT NULL,
-    CityEnglish varchar(100) NOT NULL,
-    CityFrench varchar(100) NOT NULL,
-    CountryEnglish varchar(100) NOT NULL,
-    CountryFrench varchar(100) NOT NULL,
-    RegionEnglish varchar(100) NOT NULL,
-    RegionFrench varchar(100) NOT NULL,
+    LocationKey varchar(100) PRIMARY KEY,
+    City varchar(100) NOT NULL,
+    Country varchar(100) NOT NULL,
+    Region varchar(100) NOT NULL,
     PostalCode varchar(100) NOT NULL,
 );
 
-CREATE TABLE c
+DROP TABLE IF EXISTS factSales
+CREATE TABLE factSales
 (
     SalesKey int IDENTITY(1,1) PRIMARY KEY,
     OriginalId int NOT NULL,
-    CustomerKey int NOT NULL
+    CustomerKey varchar(50) NOT NULL
     CONSTRAINT FK_CustomerKey REFERENCES dimCustomer(CustomerKey),
-    CustomerLocationKey int NOT NULL
+    CustomerLocationKey varchar(100) NOT NULL
     CONSTRAINT FK_CustomerLocationKey REFERENCES dimLocation(LocationKey),
     SellerKey int NOT NULL
     CONSTRAINT FK_SellerKey REFERENCES dimEmployee(EmployeeKey),
@@ -73,7 +86,7 @@ CREATE TABLE c
     CONSTRAINT FK_OrderDateKey REFERENCES dimDate(DateKey),
     ShipDateKey int NOT NULL
     CONSTRAINT FK_ShipDateKey REFERENCES dimDate(DateKey),
-    ShipLocationKey int NOT NULL
+    ShipLocationKey varchar(100) NOT NULL
     CONSTRAINT FK_ShipLocationKey REFERENCES dimLocation(LocationKey),
     ShipperName varchar(100) NOT NULL,
     ShipperOriginalId int NOT NULL
@@ -87,5 +100,3 @@ CREATE TABLE c
     isOnline bit NOT NULL,
 );
 
-
-select * from factSales
